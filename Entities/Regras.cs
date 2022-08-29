3668969821a46ff3ecbd6a36455ce2c311509f14
Nathalia Vieira <Silva.Nathalia@ibm.com>
2022-08-29 18:07:08 -0300

    first commit

---
+++ Entities/Regras.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace JogoInfantil
+{
+    class Regras
+    {
+        public int Teste { get; set; }
+        public string Resultado { get; set; }
+        
+        public Regras(int teste)
+        {
+            Teste = teste;
+        }
+        public void ImprimirGanhador()
+        {
+            string[] Jogador1 = new string[Teste];
+            string[] Jogador2 = new string[Teste];
+
+            for (int i = 0; i < Teste; i++)
+            {
+                Console.WriteLine("Escolha um sinal ao jogador 1 (ataque, pedra ou papel: )");
+                Jogador1[i] = Console.ReadLine();
+                Console.WriteLine("Escolha um sinal ao jogador 2 (ataque, pedra ou papel: )");
+                Jogador2[i] = Console.ReadLine();
+                
+                if (Jogador1[i] == "ataque" && Jogador2[i] == "ataque")
+                {
+                    Console.WriteLine("Aniquilacao mutua");
+                }
+                else if (Jogador1[i] == "papel" && Jogador2[i] == "papel")
+                {
+                    Console.WriteLine("Ambos venceram");
+                }
+                else if (Jogador1[i] == "pedra" && Jogador2[i] == "pedra")
+                {
+                    Console.WriteLine("Sem ganhador");
+                }
+                else if (Jogador1[i] == "ataque" && Jogador2[i] == "papel"
+                    || Jogador1[i] == "pedra" && Jogador2[i] == "papel"
+                    || Jogador1[i] == "ataque" && Jogador2[i] == "pedra")
+                {
+                    Console.WriteLine("Jogador 1 venceu");
+                }
+                else if (Jogador1[i] == "papel" && Jogador2[i] == "ataque"
+                    || Jogador1[i] == "papel" && Jogador2[i] == "pedra"
+                    || Jogador1[i] == "pedra" && Jogador2[i] == "ataque")
+                {
+                    Console.WriteLine("Jogador 2 venceu");
+                }
+                else
+                {
+                    Console.WriteLine("sinal digitado é invalido"); 
+                } 
+            }
+        }  
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace JogoInfantil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos testes vai querer fazer?");
+            var teste = 0;
+            try
+            {
+                teste = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erro, digite um numero inteiro");
+            }
+            Regras regras = new Regras(teste);
+            regras.ImprimirGanhador();
+        }
+    }
+}
